40866b953adac997d1f618f75a38fb5d1366f584
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-03 07:52:09 +0800

    009 Example Animating a Modal

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     </transition>
     <button @click="showHide">show/hide</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
